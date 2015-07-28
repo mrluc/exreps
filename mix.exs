e09bcdf27554936147552782dc24cdccbb9e1f48
Luc Fueston <superluc@gmail.com>
2015-07-28 08:50:32 -0500

    Initial commit, Mixfile

---
+++ mix.exs
@@ -0,0 +1,27 @@
+
+defmodule SpacedRepetitions.Mixfile do
+
+  use Mix.Project
+
+  @description "Rough impl of SM2 in Elixir"
+
+  @package [
+    files: [ "lib", "mix.exs", "README.md", "LICENSE.md"],
+    contributors: [ "github/mrluc" ],
+    licenses: [ "MIT" ],
+    links: %{"Github" => "https://github.com/mrluc/exreps"}
+  ]
+
+  @deps [{ :timex, "~> 0.16.0"}]
+
+  def project do
+    [
+      app:         :spaced_repetitions,
+      version:     "0.0.1",
+      elixir:      ">= 1.0.0",
+      deps:        @deps,
+      description: @description,
+      package:     @package
+    ]
+  end
+end
